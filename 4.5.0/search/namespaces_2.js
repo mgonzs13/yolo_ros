a2e7d8d1e43fa37d53c02ddd0090fb560911442d
mgonzs13 <mgonzs13@users.noreply.github.com>
2026-01-19 21:11:55 +0000

    deploy: 7ef13c99f224b5b6712354029d04024cf0d115af

---
+++ 4.5.0/search/namespaces_2.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['yolo_5fros_0',['yolo_ros',['../namespaceyolo__ros.html',1,'']]],
+  ['yolo_5fros_3a_3adebug_5fnode_1',['debug_node',['../namespaceyolo__ros_1_1debug__node.html',1,'yolo_ros']]],
+  ['yolo_5fros_3a_3adetect_5f3d_5fnode_2',['detect_3d_node',['../namespaceyolo__ros_1_1detect__3d__node.html',1,'yolo_ros']]],
+  ['yolo_5fros_3a_3atracking_5fnode_3',['tracking_node',['../namespaceyolo__ros_1_1tracking__node.html',1,'yolo_ros']]],
+  ['yolo_5fros_3a_3ayolo_5fnode_4',['yolo_node',['../namespaceyolo__ros_1_1yolo__node.html',1,'yolo_ros']]]
+];
