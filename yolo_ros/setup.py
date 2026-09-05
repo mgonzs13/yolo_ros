eb11e81f7dbdb81c23a61d743ce0bac3cf9b07eb
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-09-05 10:39:13 +0200

    new version 4.7.1

--- yolo_ros/setup.py
+++ yolo_ros/setup.py
@@ -110,7 +110,7 @@ class uv_sync_develop(UvSyncMixin, develop):
 
 setup(
     name=package_name,
-    version="4.7.0",
+    version="4.7.1",
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
